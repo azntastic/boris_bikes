52106ae7b7a2cf7ef67cb29efbf1be457b4b384a
Ryan <Ryanchu.ws@gmail.com>
2017-02-17 11:03:36 +0000

    testing branch fetch

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -11,16 +11,18 @@ class DockingStation
     @num_bikes.pop
   end
 
-  def dock_bike(bike1)
+  def dock_bike(bike)
   #nb - Bike is created outside func.; Dock capcity = 1.
   #this method will send error if @bike already exists
   #@bike will only exist if a bike has already been docked
   #hence that bike will have become instance variable
     fail "I iz full" if @num_bikes.length >= 20
-    @num_bikes << bike1
+    @num_bikes << bike
   end
 
   attr_reader :bike
   attr_reader :num_bikes
+
+#hello Alice
 
 end
